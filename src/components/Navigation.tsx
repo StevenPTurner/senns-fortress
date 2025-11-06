ced98b490ae536b3ac399e69a8e337d3664735f8
Steven Turner <steventurner2@outlook.com>
2025-11-06 16:41:31 +0000

    Added navigation without any content

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,8 @@
+// import AppBar from '@mui/material/AppBar';
+// import { Tab } from '@mui/material';
+// import TabPanel from '@mui/lab/TabPanel';
+// import { TabContext, TabList } from '@mui/lab';
+
+// export default function Navigation() {
+//     return ();
+// }
