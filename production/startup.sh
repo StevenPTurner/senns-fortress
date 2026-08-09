caaf80ce1ceca674319288a62df5352fbb5e1223
Steven Turner <steventurner2@outlook.com>
2026-08-09 16:17:16 +0100

    updated start up script to use offical docker setup for v2 plugin

--- production/startup.sh
+++ production/startup.sh
@@ -3,14 +3,27 @@
 apt-get update -y
 apt-get upgrade -y
 
-#Install Docker
-apt-get install -y docker.io
-systemctl enable docker
-systemctl start docker
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-#Install Docker compose
-curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+# Install Docker packages
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt update
 
 #Make project directory
 mkdir -p /home/ubuntu/senns-fortress
