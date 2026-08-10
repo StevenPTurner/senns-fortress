906a698f353a97de20bed80ff3d9a0a9f31192a5
Andrew Rore <Andrewrore94@gmail.com>
2026-08-10 14:33:27 +0100

    Fixing type

--- frontend/src/components/lists/SiteListItem.tsx
+++ frontend/src/components/lists/SiteListItem.tsx
@@ -1,12 +1,12 @@
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import Site from "../../types/QuizSite.types";
 import Box from "@mui/material/Box";
 import ListLogo from "./ListLogo";
 import ListLaunchButton from "./ListLaunchButton";
+import ListSite from "../../types/ListSite.types";
 
 interface SiteListItemProps {
-    listSite: Site
+    listSite: ListSite
 }
 
 export default function SiteListItem({ listSite }: SiteListItemProps) {
